50a7acb3d3e9dc3c75d5bda08bd0c501ba3153a1
HGEpro <57325674+HGEpro@users.noreply.github.com>
2022-02-15 16:40:54 +0100

    Add files via upload

---
+++ server/checker.py
@@ -0,0 +1,71 @@
+from functools import wraps
+from sanic.response import json
+
+
+
+def args_checker(needs_login):
+    def decorator(f):
+        @wraps(f)
+        async def check(request, *args, **kwargs):
+
+            if f.__name__ == "users":
+                page = request.args.get("page")
+                if page:
+                    page = int(page)
+                else:
+                    page = 0
+                if page < 0:
+                    return json({"error": "Page must be a positive integer"})
+                
+                return await f(request, *args, **kwargs)
+                
+
+            if f.__name__ == "signup":
+                username = request.args.get("username")
+                password = request.args.get("password")
+                email = request.args.get("email")
+                discord = request.args.get("discord")
+                twitter = request.args.get("twitter")
+                bio = request.args.get("bio")
+
+                if not username or not password:
+                    return json({"error": "Missing username or password"})
+
+                if len(username) > 32:
+                    return json({"error": "Username must be less than 32 characters"})
+
+                if len(password) > 64:
+                    return json({"error": "Password must be less than 64 characters"})
+
+                if email and len(email) > 320:
+                    return json({"error": "Email must be less than 320 characters"})
+
+                if discord and len(discord) > 32:
+                    return json({"error": "Discord must be less than 32 characters"})
+
+                if twitter and len(twitter) > 32:
+                    return json({"error": "Twitter must be less than 16 characters"})
+
+                if bio and len(bio) > 50:
+                    return json({"error": "Bio must be less than 50 characters"})
+                
+                return await f(request, [username, password, email, discord, twitter, bio], *args, **kwargs)
+
+        
+            if needs_login:
+                username = request.args.get("username")
+                password = request.args.get("password")
+            
+                if not username or not password:
+                    return json({"error": "Missing username or password"})
+                else:
+                    return json({"error": "Not logged in"})
+
+                return await f(request, *args, **kwargs)
+                
+            else:
+                return await f(request, *args, **kwargs)
+    
+        return check
+
+    return decorator
---
+++ server/db.py
@@ -0,0 +1,46 @@
+import aiosqlite
+import utils
+
+async def setup_tables(db):
+        async with db.cursor() as c:
+            await c.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            username VARCHAR(32) PRIMARY KEY,
+            password VARCHAR(64) NOT NULL,
+            email VARCHAR(320), 
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            discord VARCHAR(32),
+            twitter VARCHAR(16),
+            bio VARCHAR(50));
+            """)
+
+async def list_users(db, page):
+
+    temp = []
+    async with db.cursor() as c:
+        await c.execute("SELECT username FROM users LIMIT 20 OFFSET ?", (page * 20,))
+        for row in await c.fetchall():
+            temp.append(row[0])
+        
+    return temp
+
+    
+async def add_user(db, username, password, email, discord, twitter, bio):
+    async with db.cursor() as c:
+        await c.execute("""
+    INSERT INTO users (username, password, email, discord, twitter, bio)
+    VALUES (?, ?, ?, ?, ?, ?)
+    """, (username, utils.hash(password.encode("utf8")), email, discord, twitter, bio,))
+        await db.commit()
+
+
+async def find_user(db, username):
+    temp = []
+    async with db.cursor() as c:
+        await c.execute("""
+    SELECT username, email, discord, twitter, bio FROM users WHERE username LIKE ? LIMIT 10
+    """, (username,))
+        for row in await c.fetchall():
+            temp.append({"username": row[0], "email": row[1], "discord": row[2], "twitter": row[3], "bio": row[4], "profile_picture": utils.get_gravatar(row[1])})
+    
+    return temp
