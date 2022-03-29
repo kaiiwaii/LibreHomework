c7a55925ea731669bd451fda1426ba87d23d3cd3
HGEpro <tstudio2007@gmail.com>
2022-03-29 09:02:20 +0200

    fixed millis in timestamp

--- app/src/taskmanager.js
+++ app/src/taskmanager.js
@@ -2,7 +2,7 @@ import { invoke } from '@tauri-apps/api';
 
 export class Task {
     async create(name, subject, description, expires_at) {
-        return await invoke("add_task", { name: name, subject: subject, description: description, expiresAt: expires_at });
+        return await invoke("add_task", { name: name, subject: subject, description: description, expiresAt: (parseInt(expires_at) / 1000).toString() });
     }
     async get_batch(limit, page) {
         return await invoke("get_tasks", { limit: limit, page: page });
