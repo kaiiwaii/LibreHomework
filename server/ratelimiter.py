300d818badc3161fc7848e2c1566244c2bbb81e3
YeahNotSewerSide <47860375+DoctorEenot@users.noreply.github.com>
2022-04-01 02:42:05 +0300

    Wrote better rate limiter

--- server/ratelimiter.py
+++ server/ratelimiter.py
@@ -1,51 +1,70 @@
-from functools import wraps
 import time
-from sanic.response import json
 
-class RateLimiter:
-    def __init__(self):
-        self.storage = {}
+class TokenBucket:
+    '''
+    Implementation of TokenBucket
+    '''
+    def __init__(self,tokens:int,time_for_token:int):
+        '''
+        tokens:int - maximum amount of tokens
+        time_for_token: - time in which 1 token is added  
+        '''
+        self.token_koef = tokens/time_for_token
+        self.tokens = tokens
+        self.last_check = time.time()
 
-    
-    async def limit(self, calls, per_second, func, request, *args, **kwargs):
-
+    def handle(self) -> bool:
         current_time = time.time()
-        cell = self.storage.get(request.ip)
+        time_delta = current_time - self.last_check
+        self.last_check = current_time
 
-        if not cell:
-            cell = [calls-1, current_time]
-            self.storage[request.ip] = cell
-            return await func(request,*args,**kwargs)
+        self.tokens += time_delta*self.token_koef
 
-        time_delta = current_time - cell[-1]
-        to_add = int(time_delta*(calls/per_second))
-        cell[0] += to_add
+        if self.tokens > self.max_tokens:
+            self.tokens = self.max_tokens
+        
+        if self.tokens < 1:
+            return False
+        
+        self.tokens -= 1
+        return True
 
-        if cell[0] > calls:
-            cell[0] = calls
-                    
-        if cell[0] <= 0:
-            return json({"success": False, "ratelimit": True})
-
-        self.storage[request.ip][0] -= 1
-        self.storage[request.ip][1] = current_time
-        return await func(request, *args, **kwargs)
-
-
-class EndpointLimiter:
+class RateLimited(Exception):
     def __init__(self):
-        self.funcs = {}
+        super().__init__("Call was rate limited")
+
+GLOBAL_BUCKETS = {}
+
+def rate_limit(tokens:int,time_for_token:int):
+    def wrapper(function):
+        def wrapped(*args,**kwargs):
+            fn_name = function.__name__
+            try:
+                bucket = GLOBAL_BUCKETS[fn_name]
+            except:
+                bucket = TokenBucket(tokens,time_for_token)
+                GLOBAL_BUCKETS[fn_name] = bucket
+
+            if not bucket.handle():
+                raise RateLimited()
+
+            return function(*args,**kwargs)
+        return wrapped
+    return wrapper
 
 
-    def limit(self, calls, per_second):
-        def decorator(func):
-            @wraps(func)
-            async def wrapper(request, *args, **kwargs):
-                try:
-                    return await self.funcs[func.__name__].limit(calls, per_second, func, request, *args, **kwargs)
-                except KeyError:
-                    rate_limiter = RateLimiter()
-                    self.funcs[func.__name__] = rate_limiter
-                    return await self.funcs[func.__name__].limit(calls, per_second, func, request, *args, **kwargs)
-            return wrapper
-        return decorator
+
+if __name__ == "__main__":
+    @rate_limit(3,50)
+    def func1(a):
+        print("Function1",a)
+
+    for i in range(10):
+        try:
+            a = func1(555)
+        except RateLimited:
+            print("RT")
+
+        time.sleep(2)
+
+
